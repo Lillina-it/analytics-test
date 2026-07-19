7dcc91eb0b017a9438e6a3fd41c3c79f93ce48cc
Lillina-it <Kuchina06@bk.ru>
2026-07-20 00:29:36 +0300

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,6 +1,7 @@
-WITH template_events AS (
+WITH all_events AS (
     SELECT
         user_id,
+        event,
         event_time,
         value,
         LAG(event_time) OVER (
@@ -8,39 +9,44 @@ WITH template_events AS (
             ORDER BY event_time
         ) AS prev_time
     FROM logs
-    WHERE event = 'template_selected'
 ),
 
 sessions AS (
-    SELECT *,
-           SUM(
-               CASE
-                   WHEN prev_time IS NULL
-                        OR event_time - prev_time > INTERVAL '5 minutes'
-                   THEN 1
-                   ELSE 0
-               END
-           ) OVER (
-               PARTITION BY user_id
-               ORDER BY event_time
-           ) AS session_id
-    FROM template_events
+    SELECT
+        *,
+        SUM(
+            CASE
+                WHEN prev_time IS NULL
+                  OR event_time - prev_time > INTERVAL '5 minutes'
+                THEN 1
+                ELSE 0
+            END
+        ) OVER (
+            PARTITION BY user_id
+            ORDER BY event_time
+        ) AS session_id
+    FROM all_events
 ),
 
-repeated_templates AS (
-    SELECT *,
-           LAG(value) OVER (
-               PARTITION BY user_id, session_id
-               ORDER BY event_time
-           ) AS previous_template
+template_events AS (
+    SELECT
+        user_id,
+        session_id,
+        event_time,
+        value,
+        LAG(value) OVER (
+            PARTITION BY user_id, session_id
+            ORDER BY event_time
+        ) AS prev_template
     FROM sessions
+    WHERE event = 'template_selected'
 )
 
 SELECT
     value AS template_name,
     COUNT(*) AS repeat_count
-FROM repeated_templates
-WHERE value = previous_template
+FROM template_events
+WHERE value = prev_template
 GROUP BY value
 ORDER BY repeat_count DESC
 LIMIT 5;
